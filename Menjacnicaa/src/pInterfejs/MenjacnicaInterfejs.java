deec7432d83a34155779567f8f31261ee3ff4a83
markokastratovic <markokastratovic97@gmail.com>
2018-03-10 13:10:05 +0100

    Ispravljena greska u menjacnicaInterfejsu

--- Menjacnicaa/src/pInterfejs/MenjacnicaInterfejs.java
+++ Menjacnicaa/src/pInterfejs/MenjacnicaInterfejs.java
@@ -1,5 +1,5 @@
 package pInterfejs;
-import
+import menjacnica.Valuta;
 public interface MenjacnicaInterfejs {
 	void dodajKurs(String skraceniNaziv,String naziv,int dan,int mesec,int godina,double srednji,double kupovni,double prodajni);
 	void izbrisiKurs(String skraceniNaziv,String naziv,int dan,int mesec,int godina,double srednji,double kupovni,double prodajni);
