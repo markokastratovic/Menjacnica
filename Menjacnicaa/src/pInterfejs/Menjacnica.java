7ecdf221e7723ac2006bda6bf53088015be6f717
markokastratovic <markokastratovic97@gmail.com>
2018-03-10 13:51:00 +0100

    Napravljena klasa Menjacnica koja implementira menjacnicaInterfejs

--- Menjacnicaa/src/pInterfejs/Menjacnica.java
+++ Menjacnicaa/src/pInterfejs/Menjacnica.java
@@ -1,26 +1,51 @@
 package pInterfejs;
 
+import java.util.LinkedList;
+
 import menjacnica.Valuta;
 
 public class Menjacnica implements MenjacnicaInterfejs {
-
+	LinkedList<Valuta>listaValuta=new LinkedList<Valuta>();
+	
 	@Override
 	public void dodajKurs(String skraceniNaziv, String naziv, int dan, int mesec, int godina, double srednji,
 			double kupovni, double prodajni) {
-		
-
+		Valuta v=new Valuta();
+		v.setDan(dan);
+		v.setMesec(mesec);
+		v.setGodina(godina);
+		v.setKupovniKurs(kupovni);
+		v.setProdajniKurs(prodajni);
+		v.setSrednjiKurs(srednji);
+		v.setNaziv(naziv);
+		v.setSkraceniNaziv(skraceniNaziv);
+		listaValuta.add(v);
 	}
 
 	@Override
-	public void izbrisiKurs(String skraceniNaziv, String naziv, int dan, int mesec, int godina, double srednji,
-			double kupovni, double prodajni) {
-		
-
+	public void izbrisiKurs(String skraceniNaziv, int dan, int mesec, int godina) {
+		Valuta v=new Valuta();
+		v.setDan(dan);
+		v.setMesec(mesec);
+		v.setGodina(godina);
+		v.setSkraceniNaziv(skraceniNaziv);
+		if(listaValuta.contains(v)) {
+			listaValuta.remove(v);
+		}
 	}
 
 	@Override
 	public Valuta nadjiKurs(String skraceniNaziv, int dan, int mesec, int godina) {
-		
+		Valuta v=new Valuta();
+		v.setDan(dan);
+		v.setMesec(mesec);
+		v.setGodina(godina);
+		v.setSkraceniNaziv(skraceniNaziv);
+		if(listaValuta.contains(v)) {
+			int i;
+			i=listaValuta.indexOf(v);
+			return listaValuta.get(i);
+		}
 		return null;
 	}
 
