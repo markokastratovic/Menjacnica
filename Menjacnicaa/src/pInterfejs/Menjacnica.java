05a4c1fb86e2cef97868679bc5fb7f66ef83c8e2
markokastratovic <markokastratovic97@gmail.com>
2018-03-10 14:09:34 +0100

    druga implementacija za dodavanjeKursa

--- Menjacnicaa/src/pInterfejs/Menjacnica.java
+++ Menjacnicaa/src/pInterfejs/Menjacnica.java
@@ -1,13 +1,25 @@
 package pInterfejs;
 
+import java.util.LinkedList;
+
 import menjacnica.Valuta;
 
 public class Menjacnica implements MenjacnicaInterfejs {
-
+	LinkedList<Valuta>listaValuta=new LinkedList<Valuta>();
+	
 	@Override
 	public void dodajKurs(String skraceniNaziv, String naziv, int dan, int mesec, int godina, double srednji,
 			double kupovni, double prodajni) {
-		
+		Valuta v=new Valuta();
+		v.setDan(dan);
+		v.setGodina(godina);
+		v.setMesec(mesec);
+		v.setKupovniKurs(kupovni);
+		v.setProdajniKurs(prodajni);
+		v.setSrednjiKurs(srednji);
+		v.setNaziv(naziv);
+		v.setSkraceniNaziv(skraceniNaziv);
+		listaValuta.addFirst(v);
 
 	}
 
